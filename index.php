0140bf25f966cc88e967f8cfdb43136dfb9a32fc
diegoGimenez <diegob__gimenez@hotmail.com>
2015-05-03 21:38:07 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<title>diegoGimenez</title>
+
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css" />
+		<link rel="stylesheet" href="css/common.css" />
+		
+		<link rel="shortcut icon" href="img/favicon.ico" />
+
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+</head>
+
+<body>
+		<nav class="navbar navbar-inverse" style="background-image: linear-gradient(to bottom, #93C4D3 0px, #93C4D3 100%); background-color:#F0DB4F; border-color:#93C4D3;">
+			<div class="container">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="index.php" style="color:#FFFFFF;">diegoGimenez</a>
+				</div>
+			</div>
+		</nav>
+
+<div class="container">
+
+<!-- header -->
+
+
+<div class="row">	
+	<div class="col-md-12">
+		<div class="well">
+				<h1>Conteudo</h1>
+				<p>
+					Mensagem.
+				</p>
+		</div>
+	</div>
+	
+</div>
+<!-- footer -->
+			<hr/>
+			<footer>
+				<p><p>Copyright 2015 diegoGimenez™</p>
+			</footer>
+		</div> <!-- fim container -->
+		
+		<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		<script src="js/common.js"></script>
+</body>
+</html>
